08c2b9ee4832bb9a51b2b71e6b3c8fbbce205e1c
xiankuimeng <xiankuimeng@pateo.com.cn>
2016-02-23 16:10:41 +0800

    添加crashreport

--- MyMDApp/src/com/example/application/MyApplication.java
+++ MyMDApp/src/com/example/application/MyApplication.java
@@ -1,10 +1,14 @@
 package com.example.application;
 
 import com.mxk.baseapplication.LBaseApplication;
-
+import com.tencent.bugly.crashreport.CrashReport;
 
 
 public class MyApplication extends LBaseApplication
 {
-	
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CrashReport.initCrashReport(getApplicationContext(), "注册时申请的APPID", false);
+    }
 }
